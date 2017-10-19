50b3ff8a9178ff214b8efd1ff29788b81bff2057
Michael G <gmichael225@gmail.com>
2017-10-19 12:39:24 +1100

    Code

---
+++ ballots.php
@@ -0,0 +1,55 @@
+<?php
+
+  require __DIR__ . '/vendor/mike42/escpos-php/autoload.php';
+
+  $names = [];
+
+  $role = readline("Position: ");
+
+  do {
+    $name = readline("Name: ");
+    if($name) { $names[] = $name; }
+  } while ($name);
+
+  $ballots = (int) readline("Num ballots:");
+
+  use Mike42\Escpos\PrintConnectors\CupsPrintConnector;
+  use Mike42\Escpos\Printer;
+
+  try {
+    $connector = new CupsPrintConnector("ReceiptPrinter");
+    $printer = new Printer($connector);
+
+    for($i = 0; $i < $ballots; $i++) {
+
+      $printer->feed(2);
+
+      $printer->text(
+        "VOTING BALLOT\n" . 
+        "For the role of " . $role . "\n\n" .
+        "Rank the candidates in order of preference:\n" .
+        "  1 being your first preference, and \n" .
+        "  " . count($names) . " being your last preference.\n\n"
+      );
+
+
+      foreach($names as $name) {
+        $printer->text(
+          str_repeat(" ", 35) . " ----\n" .
+          str_repeat(" ", 5) . $name . str_repeat(" ", 30 - strlen($name)) . "|    |\n" .
+          str_repeat(" ", 35) . " ----\n"
+        );
+        echo $name;
+      }
+
+      $printer->feed(5);
+
+      $printer->cut();
+
+    }
+
+    $printer->close();
+
+  } catch (Exception $e) {
+    echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
+  }
